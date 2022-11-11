9b3bf51ca2cf7b643c982799c2be2a3a339d31f5
Eduardo Silva <cveduardo73@gmail.com>
2022-11-10 21:54:41 -0300

    alterações

--- src/main/java/Enum/TipoLicitacaoEnum.java
+++ src/main/java/Enum/TipoLicitacaoEnum.java
@@ -11,7 +11,7 @@ package Enum;
 public enum TipoLicitacaoEnum {
     
     TOMADA_DE_PRECOS_LEI_8666("Tomada de preços - Lei 8.666"),
-    TOMADA_DE_PRECOS_LEI_14133("Tomada de preços - Lei 14.133"),
+//    TOMADA_DE_PRECOS_LEI_14133("Tomada de preços - Lei 14.133"),
     CONVITE_LEI_8666("Convite - Lei 8.666"),
     DISPENSA_LEI_8666("Dispensa - Lei 8.666"),
     DISPENSA_LEI_14133("Dispensa - Lei 14.133"),
@@ -20,7 +20,8 @@ public enum TipoLicitacaoEnum {
     PREGÃO_LEI_14133("Pregão - Lei 14.133"),
     CONCORRÊNCIA_LEI_8666("Concorrência - Lei 8.666"),
     CONCORRÊNCIA_LEI_14133("Concorrência - Lei 14.133"),
-    CONCORRÊNCIA_LEI_13019("Concorrência - Lei 13.019"),
+     CONCORRÊNCIA_LEI_13019("Chamado público"),
+//    CONCORRÊNCIA_LEI_13019("Concorrência - Lei 13.019"),
     INEXIGIBILIDADE_LEI_8666("Inexigibilidade - Lei 8.666"),
     INEXIGIBILIDADE_LEI_14133("Inexigibilidade - Lei 14.133"),
     INEXIGIBILIDADE_LEI_13019("Inexigibilidade - Lei 13.019");
